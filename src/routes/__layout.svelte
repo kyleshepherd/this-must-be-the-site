a59fc59d43e8342a05143946775886bd88eea2bc
Kyle Shepherd <kyle@thisissoon.com>
2022-06-15 21:22:57 +0100

    feat(Setup): Add starter kit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<style lang="scss">
+  :global {
+    @import "../styles/global.scss";
+  }
+
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: $light;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+</style>
+
+<main>
+  <slot />
+</main>
