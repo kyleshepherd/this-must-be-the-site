cd31347872d4ca656fa415d59e96ced0516c5cd5
Kyle Shepherd <kyle@thisissoon.com>
2022-11-14 16:05:53 +0000

    fix(Upload): Potential fix for redirect loop

--- src/routes/upload/index.svelte
+++ src/routes/upload/index.svelte
@@ -1,15 +1,21 @@
+<script lang="ts" context="module">
+  import type { Load } from "@sveltejs/kit";
+
+  export const load: Load = async function load() {
+    if (typeof window !== "undefined" && get(account) === null) {
+      return {
+        status: 302,
+        redirect: "/login",
+      };
+    }
+    return {};
+  };
+</script>
+
 <script lang="ts">
   import { account } from "../../store/account";
   import { get } from "svelte/store";
   import Upload from "../../components/_pages/Upload/Upload.svelte";
-  import { goto } from "$app/navigation";
-  import { onMount } from "svelte";
-
-  onMount(() => {
-    if (get(account) === null) {
-      goto("/login");
-    }
-  });
 </script>
 
 <Upload />
